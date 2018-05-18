640047bf07897c2dde901438bf5c4380f8f29fd0
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-05-18 20:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const logWhisper = (string) => {
 
 const sayHiToGrandma = (string) => {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+      return "I can't hear you!"
   } else if (string === string.toUpperCase()){
       return "YES INDEED!"
   } else if (string === "I love you, Grandma"){
